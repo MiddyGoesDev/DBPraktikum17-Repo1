fc0a073f4c8437a1c54ee987e1fad93d29820c0c
5feldtma <5feldtma@informatik.uni-hamburg.de>
2017-08-08 16:50:17 +0200

    Fix distorted shapes bug

--- public/game.js
+++ public/game.js
@@ -8,12 +8,12 @@ function startGame() {
     var gameStage = new createjs.Stage('gameField');
 
     var circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(40, 40, 10);
+    circle.graphics.beginFill("red").drawCircle(100, 100, 10);
     circle.x = 100;
         circle.y = 50;
 
     var shape = new createjs.Shape();
-    shape.graphics.beginFill("#ff0000").drawRect(0, 0, 100, 100);
+    shape.graphics.beginFill("#ff0000").drawRect(50, 50, 100, 100);
 
     gameStage.addChild(circle);
     gameStage.addChild(shape);
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,7 +6,7 @@ class Game extends React.Component {
     render() {
         return (
             <div className="game">
-                <canvas id="gameField" />
+                <canvas id="gameField" width="5000" height="5000"/>
             </div>
         );
     }
