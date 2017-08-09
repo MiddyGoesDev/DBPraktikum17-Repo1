c5ad3702389f8ce3fa882555a1407ddf53437023
Middy <5rietz@informatik.uni-hamburg.de>
2017-08-09 14:36:41 +0200

    chat am implementieren mit paddy

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -5,13 +5,13 @@ class Chat extends React.Component {
     render() {
         return (
             <div className="chat-room">
-                <div className="chat-messages">
+                <div className="chat-messages" id="chat-messages">
 
                 </div>
                 <div className="chat-interface">
-                    <form>
-                        <input type="text" placeholder="Send a message"/>
-                        <input type="submit" value={"send"}/>
+                    <form onSubmit="leaveMessage(this.message.value); this.reset(); return false;">
+                        <input type="text" name="message" placeholder="Send a message"/>
+                        <button type="submit">Send</button>
                     </form>
                 </div>
             </div>
@@ -20,3 +20,14 @@ class Chat extends React.Component {
 }
 
 export default Chat
+
+/**
+<div class="jumbotron">
+    <form onsubmit="leaveMessage(this.message.value);
+    this.reset(); return false;" class="form-inline text-center container">
+    <input class="form-control" name="message" placeholder="Message">
+    <button type="submit" class="btn btn-primary">Leave Message</button>
+    </form>
+    </div>
+<div class="container"><div class="row" id="messages"></div>
+**/
