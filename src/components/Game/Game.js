4a311643cae87017bc21d717a1963f68011e9126
Middy <5rietz@informatik.uni-hamburg.de>
2017-08-09 12:48:55 +0200

    700 400 canvas

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,8 +5,8 @@ class Game extends React.Component {
 
     render() {
         return (
-            <div className="gameWindows">
-                <canvas id="gameField" width={400} height={400} />
+            <div className="gameWindow">
+                <canvas id="gameField" width={700} height={400} />
             </div>
         );
     }
