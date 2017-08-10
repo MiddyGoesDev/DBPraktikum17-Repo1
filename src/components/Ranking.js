7ddd3abeab74ba7a2cafc0413e48cbc13e94a8bd
tim <timkilian51@gmail.com>
2017-08-10 23:08:46 +0200

    Add dummy profile and ranking

---
+++ src/components/Ranking.js
@@ -0,0 +1,13 @@
+import './Ranking.css';
+import React from 'react';
+
+export default class Ranking extends React.Component {
+
+    render() {
+        return (
+            <div className="ranking">
+
+            </div>
+        );
+    }
+}
