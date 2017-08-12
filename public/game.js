76f83ea8924701bf0f017c71f3d6776deb4f9f00
tim <timkilian51@gmail.com>
2017-08-12 02:49:19 +0200

    Big refactor

--- public/game.js
+++ public/game.js
@@ -1,9 +1,6 @@
-setTimeout(() => {
-    startGame();
-});
+setTimeout(() => { resizeGame(); startGame(); });
 
 var socket = io('http://207.154.243.43');
-
 
 // Character Movement
 const KEYCODE_S = 83;
@@ -20,18 +17,106 @@ const DIRECTION_SOUTHWEST = 225;
 const DIRECTION_WEST = 270;
 const DIRECTION_NORTHWEST = 315;
 
-var rectangle;
-var gameStage;
-var gameObjects = [];
+var gameStage = null;
 var activeKeys = [];
-var activeObject = null;
+
+// initialize stage and shapes
+function startGame() {
+    document.body.onresize = resizeGame;
+
+    gameStage = new GameStage();
+    gameStage.initialize();
+
+    document.onkeydown = gameStage.keyPressed;
+    document.onkeyup = gameStage.keyReleased;
+    // Actions carried out each tick (aka frame)
+    createjs.Ticker.addEventListener("tick", () => { gameStage.update(); });
+
+    socket.on('initialize opponents', function (data) {
+        console.log('initialize opponents');
+        console.log(data);
+        for (let id in data) {
+            if (data.hasOwnProperty(id) && data[id].id !== gameStage.activeObject.id) {
+                gameStage.link(new OpponentGuy(data[id].x, data[id].y), id);
+            }
+        }
+    });
+
+    socket.on('joined', (opponent) => {
+        gameStage.link(new OpponentGuy(opponent.x, opponent.y), opponent.id);
+
+        $('#chat-messages').append('<div class="chat-message">Player ' + opponent.id + ' has joined</div>');
+    });
+
+    socket.on('left', function (opponent) {
+        gameStage.unlink(opponent.id);
+
+        $('#chat-messages').append('<div class="chat-message">Player ' + opponent.id + ' has disconnected</div>');
+    });
+}
+
+function GameStage() {
+
+    this.construct = () => {
+        this.stage = new createjs.Stage('game-field');
+    };
+
+    this.initialize = () => {
+        this.activeObject = new PlayerGuy(10, 10);
+        new Wall(200, 0);
+    };
+
+    this.update = () => {
+        this.gameObjects.forEach((gameObject) => {
+            gameObject.update();
+        });
+        this.stage.update();
+    };
+
+    this.add = (object) => {
+        this.gameObjects.push(object);
+        this.stage.addChild(object.sprite);
+    };
+
+    this.link = (object, id) => {
+        gameStage.add(object);
+        gameStage.networkObjects[id] = object;
+    };
+
+    this.unlink = (id) => {
+        gameStage.stage.removeChild(this.networkObjects[id]);
+        this.gameObjects.filter((object) => { return object.id !== id; });
+        delete this.networkObjects[id];
+    };
+
+    this.keyPressed = (event) => {
+        if (activeKeys.lastIndexOf(event.keyCode) === -1) {
+            activeKeys.push(event.keyCode);
+        }
+        this.activeObject.handleEvent();
+    };
+
+    this.keyReleased = (event) => {
+        activeKeys = activeKeys.filter((keyCode) => {
+            return keyCode !== event.keyCode;
+        });
+        this.activeObject.handleEvent();
+    };
+
+    this.stage = null;
+    this.gameObjects = [];
+    this.activeObject = null;
+    this.networkObjects = [];
+
+    this.construct();
+}
 
 function GameObject(x, y) {
 
     this.construct = () => {
         this.sprite = new createjs.Sprite(new createjs.SpriteSheet(this.data), this.type);
-        gameStage.addChild(this.sprite);
-        this.updateSpritePosition();
+        gameStage.add(this);
+        this.updatePosition(this.x, this.y);
     };
 
     this.destruct = () => {
@@ -39,23 +124,81 @@ function GameObject(x, y) {
         delete this;
     };
 
-    this.update = () => {
+    this.update = () => { };
 
+    this.handleEvent = () => { };
+
+    this.move = () => {
+        var signX = 0, signY = 0;
+
+        switch (this.direction) {
+            case DIRECTION_NORTH:     signY = -1; break;
+            case DIRECTION_NORTHEAST: signY = -1;
+            case DIRECTION_EAST:      signX = 1; break;
+            case DIRECTION_SOUTHEAST: signX = 1;
+            case DIRECTION_SOUTH:     signY = 1; break;
+            case DIRECTION_SOUTHWEST: signY = 1;
+            case DIRECTION_WEST:      signX = -1; break;
+            case DIRECTION_NORTHWEST: signX = -1; signY = -1;
+        }
+
+        this.updatePosition(this.x + signX * this.speed, this.y + signY * this.speed);
+        this.emit('change');
     };
 
-    this.emit = () => {
-
+    this.updatePosition = (x, y) => {
+        this.x = x;
+        this.y = y;
+        this.sprite.x = x;
+        this.sprite.y = y;
     };
 
-    this.handleEvent = () => {
-
+    this.emit = (action) => {
+        socket.emit(action, {
+            id: this.id,
+            x: this.x,
+            y: this.y,
+            type: this.sprite.currentAnimation,
+            direction: this.direction });
     };
 
-    this.updateSpritePosition = () => {
-        this.sprite.x = this.x;
-        this.sprite.y = this.y;
-    };
+    socket.on('update', function (object) {
+        let gameObject = gameStage.networkObjects[object.id];
+        gameObject.updatePosition(object.x, object.y);
+        gameObject.type = object.type;
+        gameObject.direction = object.direction;
+    });
 
+    /*
+     this.checkCollision = () => {
+
+     var detectedCollision;
+     var width;
+     var height;
+     var overlapThreshold = this.speed;
+
+     detectedCollision = ndgmr.checkPixelCollision(this.sprite, rectangle, 0.01, true);
+
+     if (detectedCollision !== false) {
+     width = detectedCollision.width;
+     height = detectedCollision.height;
+     if (axis === 'y') {
+     if (width >= overlapThreshold) {
+     this[axis] -= sign * height;
+     }
+     }
+     else {
+     if (height >= overlapThreshold) {
+     this[axis] -= sign * width;
+     }
+     }
+
+     }
+     this.updateSpritePosition();
+     };
+     */
+
+    this.id = Math.floor(new Date().valueOf() * Math.random());
     this.x = x;
     this.y = y;
     this.data = null;
@@ -65,13 +208,53 @@ function GameObject(x, y) {
     this.speed = 0;
     this.armor = 0;
     this.hp = 1;
-
 }
-
 
 function Character(x, y) {
 
     GameObject.call(this, x, y);
+
+    this.idle = () => {
+        if (!this.isIdling()) {
+            this.sprite.gotoAndPlay('idle');
+        }
+    };
+
+    this.punch = () => {
+        if (!this.isPunching()) {
+            this.sprite.gotoAndPlay('punch');
+            new Bullet(this.x, this.y, this.direction);
+        }
+    };
+
+    this.walk = () => {
+        if (!this.isWalking()) {
+            this.sprite.gotoAndPlay('walk');
+        }
+    };
+
+    this.isBusy = () => {
+        return this.isPunching();
+    };
+
+    this.isIdling = () => {
+        return this.sprite.currentAnimation === 'idle';
+    };
+
+    this.isPunching = () => {
+        return this.sprite.currentAnimation === 'punch';
+    };
+
+    this.isWalking = () => {
+        return this.sprite.currentAnimation === 'walk';
+    };
+
+    this.type = 'idle';
+}
+
+function PlayerGuy(x, y) {
+
+    Character.call(this, x, y);
 
     this.update = () => {
         if (!this.isBusy() && this.isWalking()) {
@@ -80,8 +263,8 @@ function Character(x, y) {
     };
 
     this.handleEvent = () => {
-        var lastKey = activeKeys[activeKeys.length -1];
-        var secondToLastKey = activeKeys[activeKeys.length -2];
+        let lastKey = activeKeys[activeKeys.length -1];
+        let secondToLastKey = activeKeys[activeKeys.length -2];
         switch (lastKey) {
             case KEYCODE_LEFT:
                 if (secondToLastKey === KEYCODE_UP) this.direction = DIRECTION_NORTHWEST;
@@ -113,129 +296,24 @@ function Character(x, y) {
                 break;
             case KEYCODE_S:
                 this.punch();
+                this.emit('change');
                 break;
             default:
                 this.idle();
+                this.emit('change');
         }
     };
 
-    this.punch = () => {
-        if (!this.isPunching()) {
-            this.sprite.gotoAndPlay('punch');
-            gameObjects.push(new Bullet(this.x, this.y, this.direction));
-        }
-    };
-
-    this.idle = () => {
-        if (!this.isIdling()) {
-            this.sprite.gotoAndPlay('idle');
-        }
-    };
-
-    this.isBusy = () => {
-        return this.isPunching();
-    };
-
-    this.isIdling = () => {
-        return this.sprite.currentAnimation === 'idle';
-    };
-
-    this.isPunching = () => {
-        return this.sprite.currentAnimation === 'punch';
-    };
-
-    this.isWalking = () => {
-        return this.sprite.currentAnimation === 'walk';
-    };
-
-    this.walk = () => {
-        if (!this.isWalking()) {
-            this.sprite.gotoAndPlay('walk');
-        }
-    };
-
-    this.setPosition = (x, y) => {
-        this.x = x;
-        this.y = y;
-        this.updateSpritePosition(x, y);
-    };
-
-    this.move = () => {
-        var signX = 0;
-        var signY = 0;
-
-        switch (this.direction) {
-            case DIRECTION_NORTH:
-                signY = -1;
-                break;
-            case DIRECTION_NORTHEAST:
-                signY = -1;
-            case DIRECTION_EAST:
-                signX = 1;
-                break;
-            case DIRECTION_SOUTHEAST:
-                signX = 1;
-            case DIRECTION_SOUTH:
-                signY = 1;
-                break;
-            case DIRECTION_SOUTHWEST:
-                signY = 1;
-            case DIRECTION_WEST:
-                signX = -1;
-                break;
-            case DIRECTION_NORTHWEST:
-                signX = -1;
-                signY = -1;
-        }
-
-        this.x += signX * this.speed;
-        this.y += signY * this.speed;
-
-        this.updateSpritePosition();
-    };
-
-    /*
-    this.checkCollision = () => {
-
-        var detectedCollision;
-        var width;
-        var height;
-        var overlapThreshold = this.speed;
-
-        detectedCollision = ndgmr.checkPixelCollision(this.sprite, rectangle, 0.01, true);
-
-        if (detectedCollision !== false) {
-            width = detectedCollision.width;
-            height = detectedCollision.height;
-            if (axis === 'y') {
-                if (width >= overlapThreshold) {
-                    this[axis] -= sign * height;
-                }
-            }
-            else {
-                if (height >= overlapThreshold) {
-                    this[axis] -= sign * width;
-                }
-            }
-
-        }
-        this.updateSpritePosition();
-    };
-    */
-
-    // Felder
-
-    this.frames = [];
-
+    let frames = [];
     for (let j = 0; j < 18; j++) {
         for (let i = 0; i < 12; i++) {
-            this.frames.push([16 + 16 * i, 16 + 16 * j, 16, 16]);
+            frames.push([16 + 16 * i, 16 + 16 * j, 16, 16]);
         }
     }
 
     this.data = {
         images: ['./assets/sprites.png'],
-        frames: this.frames,
+        frames: frames,
         animations: {
             walk: [8 * 12, 8 * 12 + 2, "walk", 0.3],
             idle: [6 * 12, 6 * 12 + 3, "idle", 0.25],
@@ -243,10 +321,41 @@ function Character(x, y) {
             runningKick: [15 * 12 + 7, 15 * 12 + 10, 'idle', 0.25]
         }
     };
-
-    this.type = 'idle';
-
     this.speed = 3;
+    this.construct();
+    this.emit('join');
+}
+
+function OpponentGuy(x, y) {
+
+    Character.call(this, x, y);
+
+    this.update = () => {
+        switch (this.type) {
+            case 'idle': this.idle(); break;
+            case 'walk': this.walk(); break;
+            case 'punch': this.punch(); break;
+        }
+    };
+
+    let frames = [];
+
+    for (let j = 0; j < 18; j++) {
+        for (let i = 0; i < 12; i++) {
+            frames.push([16 + 16 * i, 16 + 16 * j, 16, 16]);
+        }
+    }
+
+    this.data = {
+        images: ['./assets/sprites.png'],
+        frames: frames,
+        animations: {
+            walk: [8 * 12, 8 * 12 + 2, 'walk', 0.3],
+            idle: [6 * 12, 6 * 12 + 3, 'idle', 0.25],
+            punch: [15 * 12, 15 * 12 + 2, 'idle', 0.5],
+            runningKick: [15 * 12 + 7, 15 * 12 + 10, 'idle', 0.25]
+        }
+    };
 
     this.construct();
 }
@@ -255,17 +364,16 @@ function Projectile(x, y, direction) {
 
     GameObject.call(this, x, y);
 
+    this.update = () => {
+        this.move();
+    };
+
     this.direction = direction;
 }
 
 function Bullet(x, y, direction) {
 
     Projectile.call(this, x, y, direction);
-
-    this.update = () => {
-        this.x += 6;
-        this.updateSpritePosition();
-    };
 
     this.speed = 6;
     this.data = {
@@ -276,160 +384,18 @@ function Bullet(x, y, direction) {
 }
 
 function Wall(x, y) {
-    
-}
 
-function GameStage() {
+    GameObject.call(this, x, y);
 
-    this.construct = () => {
-        this.stage = new createjs.Stage('game-field');
+    this.data = {
+        images: ['./assets/brickwall.png'],
+        frames: {width: 64, height: 64, count: 1, regX: 0, regY: 0, spacing: 0, margin: 0}
     };
-
-    this.update = () => {
-        this.gameObjects.forEach((gameObject) => {
-            gameObject.update();
-        });
-        this.stage.update();
-    };
-
-    this.stage = null;
-    this.gameObjects = [];
-    this.activeObject = null;
-    this.networkObjects = [];
-
     this.construct();
 }
 
-var guys = [];
-
-var guy;
-
-var wallData = {
-    images: ['./assets/brickwall.png'],
-    frames: {width: 64, height: 64, count: 1, regX: 0, regY: 0, spacing: 0, margin: 0}
-};
-
-var wallSpriteSheet = new createjs.SpriteSheet(wallData);
-rectangle = new createjs.Sprite(wallSpriteSheet);
-
-
-// initialize stage and shapes
-function startGame() {
-    var gameField = document.getElementById('game-field');
-    var gameWindow = document.getElementById('game-window');
-    gameField.width = gameWindow.clientWidth;
-    gameField.height = gameWindow.clientHeight;
-    gameStage = new createjs.Stage('game-field');
-
-    guy = new Character(10, 10);
-    activeObject = guy;
-    rectangle.x = 200;
-    //rectangle.y = 64;
-
-    guys[guy.id] = guy;
-
-    gameObjects.push(guy);
-
-
-    gameStage.addChild(rectangle);
-    gameStage.update();
-
-    createjs.Ticker.addEventListener("tick", handleTick);
-
-    document.onkeydown = keyPressed;
-    document.onkeyup = keyReleased;
-
-    socket.emit('join', {id: guy.id, x: guy.x, y: guy.y});
-    socket.on('guy joined', function (guy) {
-        addGuy(guy);
-    });
-    socket.on('guy left', function (enemy) {
-        gameStage.removeChild(guys[enemy.id]);
-        delete guys[enemy.id];
-        $('#chat-messages').append('<div class="chat-message">Player ' + enemy.id + ' has disconnected</div>');
-    });
-    socket.on('initialize player', function (data) {
-        for (var id in data) {
-            if (data.hasOwnProperty(id)) {
-                addGuy(data[id]);
-            }
-        }
-    });
-    socket.on('update guy', function (guy) {
-        guys[guy.id].setPosition(guy.x, guy.y);
-    });
-}
-
-function keyPressed(event) {
-    if (activeKeys.lastIndexOf(event.keyCode) === -1) {
-        activeKeys.push(event.keyCode);
-    }
-    activeObject.handleEvent();
-}
-
-function keyReleased(event) {
-    activeKeys = activeKeys.filter((keyCode) => {
-        return keyCode !== event.keyCode;
-    });
-    activeObject.handleEvent();
-}
-
-
 /*
-function keyPressed(event) {
-    console.log('key pressed: ' + event.keyCode);
-    switch (event.keyCode) {
-        case KEYCODE_LEFT:
-            guy.direction = DIRECTION_WEST;
-            guy.walk();
-            break;
-        case KEYCODE_RIGHT:
-            guy.direction = DIRECTION_EAST;
-            guy.walk();
-            break;
-        case KEYCODE_UP:
-            guy.direction = DIRECTION_NORTH;
-            guy.walk();
-            break;
-        case KEYCODE_DOWN:
-            guy.direction = DIRECTION_SOUTH;
-            guy.walk();
-            break;
-        case KEYCODE_S:
-            guy.punch();
-            break;
-    }
-    gameStage.update();
-}
-
-function keyReleased(event) {
-    console.log('key released: ' + event.keyCode);
-
-    switch (event.keyCode) {
-        case KEYCODE_LEFT:
-        case KEYCODE_RIGHT:
-        case KEYCODE_UP:
-        case KEYCODE_DOWN:
-            guy.idle();
-            break;
-    }
-    socket.emit('move guy', {id: guy.id, x: guy.x, y: guy.y});
-    gameStage.update();
-}
-*/
-
 function handleTick(event) {
-    // Actions carried out each tick (aka frame)
-    gameObjects.forEach(function (gameObject) {
-        gameObject.update();
-    });
-    //console.log(!guy.isBusy() && guy.isWalking());
-    //console.log(guy.sprite.currentAnimation);
-    gameStage.update();
-    console.log(activeKeys);
-    socket.emit('move guy', {id: guy.id, x: guy.x, y: guy.y});
-
-    /*
     if (!event.paused) {
         // Actions carried out when the Ticker is not paused.
         gameStage.update();
@@ -439,14 +405,13 @@ function handleTick(event) {
             gameStage.removeChild(bullet);
         }
     }
-    */
+
 }
+*/
 
-function addGuy(guy) {
-    if (typeof(guys[guy.id]) === "undefined") {
-        guys[guy.id] = new Character(10, 10);
-        guys[guy.id].setPosition(guy.x, guy.y);
-
-        $('#chat-messages').append('<div class="chat-message">Player ' + guy.id + ' has joined</div>');
-    }
+function resizeGame() {
+    const gameField = document.getElementById('game-field');
+    const gameWindow = document.getElementById('game-window');
+    gameField.width = gameWindow.clientWidth;
+    gameField.height = gameWindow.clientHeight;
 }
