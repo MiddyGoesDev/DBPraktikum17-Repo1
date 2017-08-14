7ba1e8f751486ebb242f1236bf91bd600bb4181f
tim <timkilian51@gmail.com>
2017-08-14 17:19:32 +0200

    Fix merge conflicts

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,8 @@
 import './Profile.css';
 
-<<<<<<< HEAD
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import { db } from 'baqend/lib/baqend';
-import {getStatsKill, getStatsDeaths, getStatsExp} from '../actions/profileAction' //clearChat
-
-
-import {bindActionCreators} from 'redux'
-=======
 import React from 'react'
->>>>>>> 1160602d5004b4e6afee6b7ab494d471245a5a0f
+import {getStatsDeaths, getStatsExp} from '../actions/profileAction' //clearChat
+
 import {connect} from 'react-redux'
 
 class Profile extends React.Component {
