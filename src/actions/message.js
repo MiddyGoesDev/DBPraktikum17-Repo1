ae5eb695dd051d000ab8cc15d192ed838a779f02
Middy <5rietz@informatik.uni-hamburg.de>
2017-08-21 14:38:48 +0200

    chat serverzeit

--- src/actions/message.js
+++ src/actions/message.js
@@ -7,7 +7,6 @@ export function sendMessage(text, bool) {
         let messageObj = new db.Message({
           'name':db.User.me.username,
           'message':text,
-          'date':new Date(),
           'isJoinInfo':bool,
         });
         return messageObj.insert().then((abort) => {
@@ -30,7 +29,7 @@ export function getMessages() {
         'BAQEND': {
             type: "MESSAGES_NEXT",
             payload: (db) => {
-              return db.Message.find().ge("date", new Date().toISOString()).ascending("date").resultStream()
+              return db.Message.find().ge("createdAt", new Date().toISOString()).ascending("createdAt").resultStream()
             }
         }
     }
