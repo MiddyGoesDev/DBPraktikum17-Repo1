e2a8286f2c0ebae37c45713ad93224484f8b1687
tim <timkilian51@gmail.com>
2017-08-24 01:59:02 +0200

    Add Key and Gate, fill inventory with key

--- src/components/Profile/Inventory.js
+++ src/components/Profile/Inventory.js
@@ -4,60 +4,88 @@ import {Grid, Image, Segment} from "semantic-ui-react";
 import './Inventory.css';
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
+import Equipment from './Equipment'
 import PropTypes from 'prop-types'
+import {myCharacter, getInventory} from "../../actions/profile";
 
-class Inventory extends React.Component{
+class Inventory extends React.Component {
 
-  constructor(props) {
+    constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            src1: '',
+            src2: '',
+            src3: '',
+            src4: '',
+            src5: '',
+            src6: '',
+            src7: '',
+            src8: '',
+            src9: '',
+            src10: ''
+        }
     }
 
-  render() {
-    return(
-      <Grid centered style={{paddingLeft: '200px'},{float:'right'}}>
-      <Segment className ="Iventory">
-        <p className="item-title">Iventory</p>
-        <Grid.Row>
-            <Segment className="item-slot">1
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">2
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">3
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">4
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">5
-                <Image src=""/>
-            </Segment>
-          </Grid.Row>
-          <Grid.Row>
-            <Segment className="item-slot">6
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">7
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">8
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">9
-                <Image src=""/>
-            </Segment>
-            <Segment className="item-slot">10
-                <Image src=""/>
-            </Segment>
-        </Grid.Row>
+    componentWillMount() {
+        this.props.actions.myCharacter().then(character =>
+            this.props.actions.getInventory(character).then(inventory => {
+                console.log(inventory.length, inventory[0]);
+                let state = {};
+                for (let i=1; i<=10 && i<=inventory.length; i++) {
+                    state['src' + i] ='./assets/items/' + Equipment.capitalizeFirstLetter(Equipment.toCamelCase(inventory[i-1].item.name)) + '.png';
+                }
+                console.log('state', state);
+                this.setState(state);
+            }));
+    }
 
-        </Segment>
-      </Grid>
-    )
-  }
+    render() {
+        return (
+            <Grid centered style={{paddingLeft: '200px', float:'right'}}>
+                <Segment className="Inventory">
+                    <p className="item-title">Iventory</p>
+                    <Grid.Row>
+                        {[...Array(5)].map((x, i) => (
+                            <Segment className="item-slot">
+                                {this.state['src' + (i+1)].length===0 ? i+1 : (<Image src={this.state['src' + (i+1)]}/>)}
+                            </Segment>
+                        ))}
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Segment className="item-slot">6
+                            <Image src={this.state.src6}/>
+                        </Segment>
+                        <Segment className="item-slot">7
+                            <Image src={this.state.src7}/>
+                        </Segment>
+                        <Segment className="item-slot">8
+                            <Image src={this.state.src8}/>
+                        </Segment>
+                        <Segment className="item-slot">9
+                            <Image src={this.state.src9}/>
+                        </Segment>
+                        <Segment className="item-slot">10
+                            <Image src={this.state.src10}/>
+                        </Segment>
+                    </Grid.Row>
 
+                </Segment>
+            </Grid>
+        )
+    }
 }
 
-export default connect(null, null)(Inventory);
+Inventory.propTypes = {
+    action: PropTypes.object
+};
+
+function mapStateToProps(state) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {actions: bindActionCreators({myCharacter, getInventory}, dispatch)};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Inventory);
+
