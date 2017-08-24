12e679c43fce99d539b38fddbb7df79f099f7150
Middy <5rietz@informatik.uni-hamburg.de>
2017-08-24 16:21:27 +0200

    comments for actions/messages.js

--- src/actions/message.js
+++ src/actions/message.js
@@ -1,6 +1,10 @@
 import {MESSAGE_SEND, MESSAGES_NEXT} from './types';
 
-
+/*
+* Sends a message which will be seen by everyone in the Chat. The Message gets inserted into the db.
+* @param test: the message to be inserted into the db
+* @return inserts the message into the db
+*/
 export function sendMessage(text) {
   return {
     'BAQEND': {
@@ -20,10 +24,12 @@ export function sendMessage(text) {
   }
 }
 
-//resultStream macht das immer wenn sich die List in DB ändert wir sie Übergeben bekommen
-//.ge("date", new Date().toISOString())
-//return db.Message.find().ascending("date").resultStream()
 
+/*
+* Gets all the messages that are the players has displayed in his chat. resultStream() updates the list with the messages
+* everytime it changes, comes from baqend realtime
+* @return finds all the messages that have been written after the user switched to the game component
+*/
 export function getMessages() {
     return {
         'BAQEND': {
