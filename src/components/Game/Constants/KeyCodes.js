3a486db2645308212f5d7f8945052d83d781917a
5feldtma <5feldtma@informatik.uni-hamburg.de>
2017-08-24 17:02:15 +0200

    Add 'press k to kill yourself'!

--- src/components/Game/Constants/KeyCodes.js
+++ src/components/Game/Constants/KeyCodes.js
@@ -1,6 +1,8 @@
-export const KEYCODE_1 = 49;
-export const KEYCODE_S = 83;
 export const KEYCODE_LEFT = 37;
 export const KEYCODE_RIGHT = 39;
 export const KEYCODE_UP = 38;
 export const KEYCODE_DOWN = 40;
+export const KEYCODE_1 = 49;
+export const KEYCODE_A = 65;
+export const KEYCODE_K = 75;
+export const KEYCODE_S = 83;
