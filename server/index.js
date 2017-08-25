4a52e691121cbe49a97944c90e9ef9af5b67c18d
tim <timkilian51@gmail.com>
2017-08-26 00:23:31 +0200

    Comment socket

--- server/index.js
+++ server/index.js
@@ -1,29 +1,40 @@
 var fs = require('fs');
 var db = require('baqend');
-var app = require('http').createServer((request, result) => fs.readFile(__dirname + '/index.html', (err, data) => {
-    if (err) {
-        result.writeHead(500);
-        return result.end('Error loading index.html');
-    }
-    result.writeHead(200);
-    result.end(data);
-}));
+// creates a node server
+var app = require('http').createServer((request, result) =>
+    // try to read index to get baqend library.
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+        if (err) {
+            result.writeHead(500);
+            return result.end('Error loading index.html');
+        }
+        result.writeHead(200);
+        result.end(data);
+    }));
 var io = require('socket.io')(app);
 var port = 8080;
 
+// connect to baqend
 db.connect('black-water-73').then(() => console.log('Connected to Baqend'));
 
 app.listen(port, function () {
     console.log('listening on *:' + port);
 });
 
+// array-object of all characters playing (sockerId => character)
 var characters = {};
+// array-object of all objects on server (objectId => object)
 var objects = {};
-
+// array of all cows on server
 var cows = [];
+// count of alive cows
 var aliveCows = 0;
+// count of max cows
 var maxCows = 10;
+// TODO define a spawnzone table in baqend
+// spawnzone of cows
 var cowZone = {x: 800, y: 3300, width: 1100, height: 700};
+
 var dragon = {
     id: generateId(),
     type: 'Dragon',
@@ -35,6 +46,7 @@ var dragon = {
     aggro: {}
 };
 objects[dragon.id] = dragon;
+
 var gate = {
     id: generateId(),
     type: 'Gate',
@@ -44,11 +56,15 @@ var gate = {
 };
 objects[gate.id] = gate;
 
+// generate all cows
 for (var i = 0; i < maxCows; i++) {
     deliverCow();
     aliveCows++;
 }
 
+/**
+ *  creates a random cow in cowZone
+ */
 function deliverCow() {
     var dx = (Math.floor(Math.random() * 10000) % 3) - 1;
     var cow = {
@@ -66,8 +82,10 @@ function deliverCow() {
     return cow;
 }
 
+// when a player connects to the server
 io.on('connection', socket => {
 
+    // initialize the character
     socket.on('join', character => {
         console.log('character ' + character.id + ' joined');
 
@@ -83,67 +101,83 @@ io.on('connection', socket => {
         }
     });
 
+    // remove character from lists
     socket.on('disconnect', () => {
+        delete objects[characters[socket.id].id];
         delete characters[socket.id];
         console.log(socket.id + ' is disconnected');
     });
 
+    // add an object to server
     socket.on('add', object => {
         objects[object.id] = object;
     });
 
+    // tell everyone that you punched
     socket.on('punch', object => {
         socket.broadcast.emit('spawn fist', object);
     });
 
+    // tell everyone that you used a weapon
     socket.on('use', object => {
         socket.broadcast.emit('spawn weapon', object);
     });
 
+    // tell everyone that something changed, and they have to update
     socket.on('change', object => {
         objects[object.id] = object;
         socket.broadcast.emit('update', object);
     });
 
+    // destroy an object on server
     socket.on('destroy', object => {
         delete objects[object.id];
     });
 
+    // increases the cow-kills-count, spawns a new cow after 15 sec and drops an item for all
     socket.on('cow died', cow => {
         if (cow.hitter === characters[socket.id].id) {
             console.log('cow died: ' + cow.id);
             cows = cows.filter(object => object.id !== cow.id);
-
             delete objects[cow.id];
-            setTimeout(() => socket.emit('spawn', deliverCow()), 15000);
+
+            // deliver a new cow
+            setTimeout(() => io.emit('spawn', deliverCow()), 15000);
+
+            // increases the killcount
             db.Statistic.find().equal('character', cow.hitter).singleResult().then(statistic => {
                 statistic.kills++;
                 statistic.update();
             });
+
+            // drop an item
             var queryBuilder = db.ItemMask.find();
-            queryBuilder.or(queryBuilder.equal('type', 'main_hand'), queryBuilder.equal('type', 'head')).resultList().then(items => {
-                var randomItem = items[Math.floor(items.length * Math.random())];
-                socket.emit('drop loot', {
-                    x: cow.x + 16,
-                    y: cow.y + 16,
-                    item: {
-                        name: randomItem.name,
-                        type: randomItem.type,
-                        vitality: roll(randomItem, 'vitality'),
-                        dexterity: roll(randomItem, 'dexterity'),
-                        strength: roll(randomItem, 'strength'),
-                        intelligence: roll(randomItem, 'intelligence'),
-                    }
+            queryBuilder.or(queryBuilder.equal('type', 'main_hand'), queryBuilder.equal('type', 'head')).resultList()
+                .then(items => {
+                    var randomItem = items[Math.floor(items.length * Math.random())];
+                    io.emit('drop loot', {
+                        x: cow.x + 16,
+                        y: cow.y + 16,
+                        item: {
+                            name: randomItem.name,
+                            type: randomItem.type,
+                            vitality: roll(randomItem, 'vitality'),
+                            dexterity: roll(randomItem, 'dexterity'),
+                            strength: roll(randomItem, 'strength'),
+                            intelligence: roll(randomItem, 'intelligence'),
+                        }
+                    });
+                    // TODO: drop gold
+                    io.emit('drop gold', {
+                        x: cow.x + (Math.ceil(32 * Math.random()) - 16),
+                        y: cow.y + (Math.ceil(32 * Math.random()) - 16),
+                        amount: Math.ceil(10 * Math.random())
+                    });
                 });
-                socket.emit('drop gold', {
-                    x: cow.x + (Math.ceil(32 * Math.random()) - 16),
-                    y: cow.y + (Math.ceil(32 * Math.random()) - 16),
-                    amount: Math.ceil(10 * Math.random())
-                });
-            });
         }
     });
 
+    // set aggro on player, when he hit a cow
     socket.on('hit cow', cow => {
         try {
             if (cow.hitter === characters[socket.id].id) {
@@ -167,6 +201,7 @@ io.on('connection', socket => {
 
     });
 
+    // open the gate for everyone
     socket.on('open gate', () => {
         if (!gate.opened) {
             gate.opened = true;
@@ -186,7 +221,7 @@ io.on('connection', socket => {
 io.emit('socket up');
 
 /**
- * Timeout player not playing
+ * Timeout not playing characters
  */
 function checkTimeout() {
     db.Character.find().equal('playing', true).resultList().then(result => {
@@ -200,10 +235,20 @@ function checkTimeout() {
 setInterval(checkTimeout, 300000);
 setTimeout(checkTimeout, 10000);
 
+/**
+ * roll a attribute of an item
+ * @param item
+ * @param attribute
+ * @returns {number}
+ */
 function roll(item, attribute) {
     return item['min_' + attribute] + Math.ceil((item['max_' + attribute] - item['min_' + attribute]) * Math.random());
 }
 
+/**
+ * Moves the cows randomly in random position when idling
+ * TODO: remove interval when cow gets aggro
+ */
 /*
  cows.forEach(cow => setInterval(() => {
  var distance = (Math.floor(Math.random() * 20) - 10);
@@ -232,20 +277,20 @@ function roll(item, attribute) {
  }, 5000 + Math.floor(Math.random() * 5000)));
  */
 
+/**
+ * generate a random id, like in GameObject
+ * @returns {number}
+ */
 function generateId() {
     return Math.floor(new Date().valueOf() * Math.random());
 }
 
+/**
+ * Gets the name of direction signs (Direction.js)
+ * @param x sign -1 0 1
+ * @param y sign -1 0 1
+ * @returns {string}
+ */
 function directionName(x, y) {
     return (y < 0 ? 'North' : y > 0 ? 'South' : '') + (x < 0 ? 'West' : x > 0 ? 'East' : '');
-}
-
-function getDirection(x, y, destX, destY) {
-    var signX = (destX - x) / Math.max(Math.abs(destX - x), 1);
-    var signY = (destY - y) / Math.max(Math.abs(destY - y), 1);
-    return {
-        x: signX,
-        y: signY,
-        name: directionName(signX, signY)
-    };
 }
