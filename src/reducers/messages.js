02ddac80803266cf008c8350b0657de5aa0e4cd5
trew26 <patmalfa@gmx.de>
2017-08-25 12:06:14 +0200

    even more comments

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -3,7 +3,10 @@ import {MESSAGE_SEND, MESSAGES_NEXT} from '../actions/types';
 const initialState = {
   list: []
 };
-
+/**
+*case MESSAGE_SEND returns the message, that'll be send
+*case MESSAGE_NEXT returns the list with every message
+*/
 export default function messages(state = initialState, action = {}) {
   switch (action.type) {
     case MESSAGE_SEND:
